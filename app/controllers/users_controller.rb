deb1ff4fcb47b3b36c70880473a00abeb13d7301
banrihayashi <banricph@gmail.com>
2018-01-02 15:05:01 +0000

    user-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = 'ユーザを登録しました。'
+      flash[:success] = '登録完了！'
       redirect_to @user
     else
-      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      flash.now[:danger] = '登録失敗！'
       render :new
     end
   end
